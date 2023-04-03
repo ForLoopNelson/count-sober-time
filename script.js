67b22049a13b2ccf91755dcddf5e22f002565f57
Cory Nelson <fullnelsontech@gmail.com>
2023-04-03 16:25:36 -0400

    finished version. second commit

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-text_field = document.querySelector('text-field')
+textField = document.querySelector('.text-field')
+
+let value = 0
+if(localStorage.getItem('value') != null) {
+  value = localStorage.getItem('value')
+textField.innerHTML = localStorage.getItem('value')
+}
+
+document.querySelector('.up-btn').addEventListener('click', () => {
+  value++
+
+  localStorage.setItem('value', value)
+textField.innerHTML = localStorage.getItem('value')
+})
+
+document.querySelector('.down-btn').addEventListener('click', () => {
+  value--
+
+  localStorage.setItem('value', value)
+textField.innerHTML = localStorage.getItem('value')
+})
+
+document.querySelector('.reset-btn').addEventListener('click', () => {
+  value = 0
+
+  localStorage.setItem('value', value)
+textField.innerHTML = localStorage.getItem('value')
+})
+
+
