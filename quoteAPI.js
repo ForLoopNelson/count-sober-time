03d76160fa532793ae96f3094957a17bc3ad17b0
Cory Nelson <fullnelsontech@gmail.com>
2023-04-06 12:36:42 -0400

    made it a set array of messages. change from test messages

--- quoteAPI.js
+++ quoteAPI.js
@@ -1,20 +1,15 @@
-const quoteText = document.querySelector('.quote-text')
-const newQuoteBtn = document.querySelector('.new-quote-btn')
 
-newQuoteBtn.addEventListener('click', getQuote)
-getQuote()
 
-function getQuote() {
-  fetch('https://icanhazdadjoke.com/',{
-    headers: {
-      'Accept' : 'application/json'
-    }
-    
-}).then(function(response) {
-  return response.json()
-}).then(function(data) {
-  const quote = data.quote
-  quoteText.innerText = quote
-})
+const messages = ['Hello!', 'How are you?', 'Good morning!', 'Nice to meet you.'];
 
-}
+const getQuote = document.querySelector('.new-quote-btn');
+const messageContainer = document.querySelector('.quote-text');
+
+getQuote.addEventListener('click', function() {
+  // Get a random message from the array
+  const randomIndex = Math.floor(Math.random() * messages.length);
+  const randomMessage = messages[randomIndex];
+
+  // Display the random message in the message container
+  messageContainer.textContent = randomMessage;
+});
